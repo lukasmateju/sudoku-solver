dd22af32f760deb87b8e2a8f46b52444b17c206a
Lukas Mateju <lmateju@umich.edu>
2026-05-21 21:23:11 -0400

    Implementaion of solver base class and removal of .cpp file for solver since it was unneeded

--- core/solver.h
+++ core/solver.h
@@ -3,6 +3,46 @@
 
 #pragma once
 
+#include <string>
+#include <vector>
+
+#include "board.h"
+
+enum actionType {
+    TRY_VALUE,
+    PLACE_VALUE,
+    REMOVE_VALUE,
+    BACKTRACK,
+    SOLVED
+};
+
+struct Step {
+    int row = 0;
+    int col = 0;
+    int value = 0; // 0 is EMPTY
+    actionType action;
+};
+
+struct Result {
+    int totalSteps = 0;
+    bool solved = false;
+    Board finalBoard;
+    std::vector<Step> stepPath;
+};
+
+struct Options {
+    bool collectSteps = false;
+    int maxSteps = 1000000;
+};
+
+
+class Solver {
+public:
+    virtual ~Solver() = default;
+
+    virtual std::string name() const = 0;
+    virtual Result solve(Board puzzle, const Options& options) = 0;
+};
 
 
 
